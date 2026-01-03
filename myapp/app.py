c214e1121941ac75b464bc965f2156d1de5ba7a7
mhassanif <mhassanimranf@gmail.com>
2026-01-03 07:36:06 +0500

    Fixed all linting violations

--- myapp/app.py
+++ myapp/app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    unused_var = "This triggers flake8"  # Unused variable
-    x=1+2  # Missing spaces around operators
     return "Hello from main page"
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-
-# Too many blank lines below (also a violation)
